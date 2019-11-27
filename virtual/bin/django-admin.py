0222957f0a079e3634fc24608d2eda6ab0d32c97
Jay-68 <kelvinngari1995@gmail.com>
2019-11-27 08:59:11 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/James/Moringa_School_projects/neighbourhood/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
