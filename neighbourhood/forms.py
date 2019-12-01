5a51564e54d91775342e0fb7ac813a9e07d0a9b6
Jay-68 <kelvinngari1995@gmail.com>
2019-12-01 12:09:29 +0300

    register models to application and create forms

---
+++ neighbourhood/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import *
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['neighbourhood', 'user']
+
+
+class NeighbourhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = []
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'neighbourhood']
