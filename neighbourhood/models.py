6ab54db7d766ad2ae69f79b599967bf9361ffc5d
Jay-68 <kelvinngari1995@gmail.com>
2019-12-01 11:47:22 +0300

    Create a final migration of the model changes

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Neighbourhood(models.Model):
+    hood_name = models.CharField(max_length=60)
+    hood_location = models.CharField(max_length=60)
+    occupants = models.IntegerField(default=0)
+
+    def __str__(self):
+      return self.hood_name
