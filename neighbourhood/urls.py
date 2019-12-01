3f43218ba8f11f1f2a144946d6aebbbc2589e7f9
Jay-68 <kelvinngari1995@gmail.com>
2019-12-01 13:07:49 +0300

    Add urls paths for the various view functions created

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search$', views.search_results, name='search'),
+    url(r'^business$', views.business, name='business'),
+    url(r'^post/(?P<post_id>\d+)/$', views.post, name='post'),
+    url(r'^profile/(?P<username>\w{0,50})$', views.profile, name='profile'),
+    url(r'^profile/edit/(?P<username>\w{0,50})$',
+        views.edit_profile, name='edit_profile'),
+    url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'^business/new/$', views.new_business, name='new_business')
 ]
