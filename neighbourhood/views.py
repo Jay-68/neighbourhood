6e57b5e655f8210f6d84763897d415843d22c5b5
Jay-68 <kelvinngari1995@gmail.com>
2019-12-01 12:16:01 +0300

    Create a main view function and its url route

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,8 +1,25 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login')
 def index(request):
-  return render(request,'index.html')
+    user = request.user
+    try:
+        profile = Profile.objects.get(user=user)
+
+    except:
+        profile = Profile.objects.create(user=user, name=user.username)
+        profile.save()
+        return redirect('edit_profile', username=user.username)
+
+    posts = Post.objects.filter(neighbourhood=profile.neighbourhood)
+    businesses = Business.objects.filter(neighbourhood=profile.neighbourhood)
+    neighbourhood = profile.neighbourhood
+
+    return render(request, 'index.html', {"posts": posts, "profile": profile, "businesses": businesses, "neighbourhood": neighbourhood})
